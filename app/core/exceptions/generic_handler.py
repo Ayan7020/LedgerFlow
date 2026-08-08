d5478cea95cb088d5829c6339b6b748131238048
Ayan7020 <ayanshaikh.pr@gmail.com>
2026-08-08 16:26:18 +0530

    Fix: added exception handler for the default 404 error

--- app/core/exceptions/generic_handler.py
+++ app/core/exceptions/generic_handler.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,Request
+from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from .app_exeception import AppException
 
@@ -16,6 +17,17 @@ def register_generic_handlers(app: FastAPI,logger: Logger):
                 "data": exc.details,
             },
         )
+
+    @app.exception_handler(404)
+    async def route_not_found_handler(request: Request, exc: HTTPException):
+        return JSONResponse(
+            status_code=404,
+            content={
+                "success": False, 
+                "message": f"Route '{request.url.path}' not found", 
+                "data": {}
+        },
+    )
 
     @app.exception_handler(Exception)
     async def unhandled_exception_handler(request: Request,exc: Exception):
