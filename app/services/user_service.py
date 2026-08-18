5893efe98ec18d10d157738b40ff85ce9e77a4ec
aamir-office <aamir@atlsystems.co.uk>
2026-08-18 18:45:47 +0530

    refactor: add tracing to userService module

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,5 +1,8 @@
-from app.models.Interfaces import IUserRepository
+from app.core import app_logger
 from app.core.exceptions import UnauthorizedException
+from app.core.observability import tracing
+
+from app.models.Interfaces import IUserRepository
 
 class UserService:
     def __init__(
@@ -8,8 +11,10 @@ class UserService:
         ):
         self.__user_repo = user_repo
 
+    @tracing("UserService.get_me")
     async def get_me(self,user_id: str):  
         user = await self.__user_repo.get_by_user_id(user_id)
         if user == None:
+            app_logger.warning("User not found")
             raise UnauthorizedException("User not found")  
         return user
